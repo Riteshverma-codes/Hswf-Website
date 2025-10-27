85ee41aefffde90f1c8691b1a5b1ab33fbf7a4f3
riteshvermacodes <riteshverma.codes@gmail.com>
2025-10-27 21:45:18 +0530

    Adding website to github

---
+++ src/Pages/Programs.jsx
@@ -0,0 +1,14 @@
+import Endurance from '../Components/Endurance'
+import PageBanner from '../components/PageBanner'
+function Programs() {
+  return (
+    <div>
+        <PageBanner 
+            title="Endurance Network" 
+            description="Connect and grow with our extensive network of partners and professionals" 
+        />
+        <Endurance />
+    </div>
+  )
+}
+export default Programs
